a7caf2005bd11dfa0a2bd86927011614a9adf406
a.keyghobadi <a123@s456>
2024-05-12 14:27:45 +0330

    3

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,41 @@
-<script setup>
+<script>
 </script>
 
 <template>
-  <header>
 
-  </header>
-  <main>
-  </main>
+  <div class="px-7 py-3">
+    <header class="max-w-screen-xl mx-auto ">
+      <div class="w-full h-14 flex justify-end items-center">
+        <div class="w-7 h-7 flex items-center">
+          <img class="w-full" src="./assets/icons/danger.gif" alt="person-icon">
+        </div>
+        <div class="w-8 h-8 mr-1">
+          <img src="./assets/icons/person-circle.svg" alt="person-icon">
+        </div>
+      </div>
+      <div class="h-2 shadow-bottom"></div>
+    </header>
+
+    <div class="grid grid-cols-5 bg-blue-100">
+
+      <aside class="col-span-1 pr-10 bg-amber-200">
+        <div class="">
+          <div class="h-10 w-10"></div>
+          <div class="">
+            <p>نام شخص</p>
+            <p>نام شرکت</p>
+          </div>
+        </div>
+      </aside>
+
+      <main class="col-span-4 bg-red-200">
+      </main>
+
+
+    </div>
+
+  </div>
+
 </template>
 
 <style scoped>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}'
+  ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Vazirmatn', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        lightGray: '#091E4200'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
-
